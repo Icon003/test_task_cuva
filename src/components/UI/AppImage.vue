1f552ae286115b60c8caf6e56ae8214745f11a0f
Бурбас Иван <Icon003@yandex.by>
2021-10-03 17:47:07 +0300

    Создание UI компонента изображения

---
+++ src/components/UI/AppImage.vue
@@ -0,0 +1,25 @@
+<template>
+	<img class="app-image" src="@/assets/image/hotel/hotel.jpg" :alt="alt" />
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({
+	name: "AppImage"
+})
+export default class AppImage extends Vue {
+	@Prop() private alt!: string;
+}
+</script>
+
+<style lang="scss">
+.app-image {
+	width: 100%;
+
+	border-radius: inherit;
+
+	object-fit: cover;
+}
+
+</style>
