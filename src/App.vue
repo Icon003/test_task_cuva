c86beb416d80acaf7fcc770c6547ed621ff7b194
Бурбас Иван <Icon003@yandex.by>
2021-10-03 17:06:16 +0300

    Подготовка базы проекта

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<div id="app">
+		<div class="main-container">
+			<router-view />
+		</div>
+	</div>
+</template>
+
+<style lang="scss">
+* {
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="home-page">
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class Home extends Vue {
+}
+</script>
+
+<style lang="scss">
+.home-page {
+}
+</style>
