560b67b78fff9d3cda92c8310ca6661bb088ef31
Бурбас Иван <Icon003@yandex.by>
2021-10-03 17:55:23 +0300

    Добавление компонента логотипа

---
+++ src/components/Logo.vue
@@ -0,0 +1,31 @@
+<template>
+	<a class="logo" href="/" @click.prevent="goToHomePage">
+		<img class="logo__image" src="@/assets/logo.png" alt="Логотип Cuva" />
+	</a>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({
+	name: "Logo"
+})
+export default class Logo extends Vue {
+	public goToHomePage() {
+		if (this.$route.path !== "/") {
+			this.$router.push("/");
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+.logo {
+	display: flex;
+
+	.logo__image {
+		height: 36px;
+	}
+}
+
+</style>
