2ca6079f6ed537ad46b5106903e24621718f4bb6
Бурбас Иван <Icon003@yandex.by>
2021-10-03 18:19:02 +0300

    Написание стора для получения отелей

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,38 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios"
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-	state: {},
-	mutations: {},
-	actions: {},
-	getters: {},
+	state: {
+		hotels: [],
+	},
+	mutations: {
+		// Mutation для сохранения отелей
+		saveHotels(state, value) {
+			state.hotels = value
+		},
+	},
+	actions: {
+		// Action для получения отелей
+		async getHotels({commit}) {
+			const currentLimit = 15
+			try {
+				const response = await axios.get("http://jsonplaceholder.typicode.com/posts", {
+					params: {
+						_limit: currentLimit
+					}
+				})
+				commit("saveHotels", response?.data)
+			} catch (error) {
+				console.error(error)
+			}
+		},
+	},
+	getters: {
+		// Getter для получения списка отелей
+		GET_HOTELS: state => state.hotels,
+	},
 	modules: {}
 });
